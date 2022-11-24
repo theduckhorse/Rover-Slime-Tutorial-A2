bb77c02db4bf054ed5b5394400a21b63f085003b
Carlsen Teo <uberstrike.newbie9999@gmail.com>
2022-11-24 17:14:59 +0800

    Add Bluetooth Serial

---
+++ comms/bluetooth_serial/bluetooth_serial.ino
@@ -0,0 +1,45 @@
+//This example code is in the Public Domain (or CC0 licensed, at your option.)
+//By Evandro Copercini - 2018
+//
+//This example creates a bridge between Serial and Classical Bluetooth (SPP)
+//and also demonstrate that SerialBT have the same functionalities of a normal Serial
+
+#include "BluetoothSerial.h"
+#include <M5StickCPlus.h>
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
+
+String message = "";
+char incomingChar;
+String temperatureString = "";
+
+void setup() {
+  M5.begin();
+  M5.Lcd.print("Hello World");
+
+  Serial.begin(115200);
+  SerialBT.begin("ESP32test"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+}
+
+void loop() {
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  }
+  if (SerialBT.available()) {
+    char incomingChar = SerialBT.read();
+    if (incomingChar != '\n'){
+      message += String(incomingChar);
+    }
+    else{
+      message = "";
+    }
+    Serial.write(incomingChar);
+    SerialBT.write(incomingChar);
+  }
+  delay(10);
+}
