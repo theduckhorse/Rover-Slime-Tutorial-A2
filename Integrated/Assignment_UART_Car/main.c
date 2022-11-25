ba629a024ae76e53669d90bdb0378c07ccb8e5c3
Hong Sheng <hongsheng322@gmail.com>
2022-11-25 14:15:46 +0800

    integrated project

---
+++ Integrated/Assignment_UART_Car/main.c
@@ -0,0 +1,74 @@
+
+/* DriverLib Includes */
+#include "driverlib.h"
+
+/* Standard Includes */
+#include <stdint.h>
+#include <stdbool.h>
+
+void uPrintf(unsigned char * TxArray);
+
+//![Simple UART Config]
+/* UART Configuration Parameter. These are the configuration parameters to
+ * make the eUSCI A UART module to operate with a 9600 baud rate. These
+ * values were calculated using the online calculator that TI provides at:
+ * http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSP430BaudRateConverter/index.html
+ */
+const eUSCI_UART_Config uartConfig =
+{
+        EUSCI_A_UART_CLOCKSOURCE_SMCLK,                 // SMCLK Clock Source
+        19,                                             // BRDIV = 19
+        8,                                              // UCxBRF = 8
+        85,                                             // UCxBRS = 85
+        EUSCI_A_UART_NO_PARITY,                         // NO Parity
+        EUSCI_A_UART_LSB_FIRST,                         // LSB First
+        EUSCI_A_UART_ONE_STOP_BIT,                      // One stop bit
+        EUSCI_A_UART_MODE,                              // UART mode
+        EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION,  // Oversampling
+};
+//![Simple UART Config]
+
+int main(void)
+{
+    /* Halting WDT  */
+    WDT_A_holdTimer();
+
+    /* Selecting P3.2 and P3.3 in UART mode */
+    GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P3, GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
+
+    /* Configuring UART Module */
+    UART_initModule(EUSCI_A2_BASE, &uartConfig);
+
+    /* Enable UART module */
+    UART_enableModule(EUSCI_A2_BASE);
+
+    /* Enabling interrupts (Rx) */
+    UART_enableInterrupt(EUSCI_A2_BASE, EUSCI_A_UART_RECEIVE_INTERRUPT);
+    Interrupt_enableInterrupt(INT_EUSCIA2);
+    Interrupt_enableMaster();
+
+    uPrintf("Comms Test\n\r");
+
+    while(1)
+    {
+        PCM_gotoLPM3InterruptSafe();
+    }
+}
+
+void uPrintf(unsigned char * TxArray)
+{
+    unsigned short i = 0;
+    while(*(TxArray+i))
+    {
+        UART_transmitData(EUSCI_A2_BASE, *(TxArray+i));  // Write the character at the location specified by pointer
+        i++;                                             // Increment pointer to point to the next character
+    }
+}
+
+/* EUSCI A2 UART ISR */
+void EUSCIA2_IRQHandler(void)
+{
+    unsigned char a = 0;
+
+    a = UART_receiveData(EUSCI_A2_BASE);
+}
