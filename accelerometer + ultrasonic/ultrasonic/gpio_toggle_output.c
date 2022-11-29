9e7c63126909678455dd62ef3ed897b6edf4afc1
PohKaiBoon <2717893P@student.gla.ac.uk>
2022-11-29 19:06:30 +0800

    Update gpio_toggle_output.c

--- accelerometer + ultrasonic/ultrasonic/gpio_toggle_output.c
+++ accelerometer + ultrasonic/ultrasonic/gpio_toggle_output.c
@@ -1,268 +1,178 @@
-//#include "driverlib.h"
-//
-//#define MIN_DISTANCE 15.0f // Distance of 15cm
-//
-//
-//bool start_pulse=0;
-//float dist;
-//
-//const Timer_A_ContinuousModeConfig continuousModeConfig =
-//   {
-//            TIMER_A_CLOCKSOURCE_SMCLK,           // SMCLK Clock Source
-//            TIMER_A_CLOCKSOURCE_DIVIDER_3,       // SMCLK/1 = 3MHz
-//            TIMER_A_TAIE_INTERRUPT_DISABLE,      // Disable Timer ISR
-//            TIMER_A_DO_CLEAR                   // Skip Clear Counter
-//   };
-//
-//   const Timer_A_CompareModeConfig CCR1Config =
-//   {
-//           TIMER_A_CAPTURECOMPARE_REGISTER_1,          // Use CCR2
-//           TIMER_A_CAPTURECOMPARE_INTERRUPT_DISABLE,   // Disable CCR interrupt
-//           TIMER_A_OUTPUTMODE_SET_RESET,
-//           0x000A                                // 10 Duty Cycle
-//   };
-//
-//// -------------------------------------------------------------------------------------------------------------------
-//   void Initalise_HCSR04(void)
-//   {
-//
-//             MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P7, GPIO_PIN7, GPIO_PRIMARY_MODULE_FUNCTION); // To set the timer to trigger echo burst every 10 µs
-//
-//
-//
-//             MAP_GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0); // To light up LED when lesser than minimum distance
-//
-//             /* Configuring Timer_A0 for continuous Mode */
-//             MAP_Timer_A_initCompare(TIMER_A1_BASE, &CCR1Config);
-//             MAP_Timer_A_configureContinuousMode(TIMER_A1_BASE, &continuousModeConfig); // 60 miliseconds measurement cycle, to easily divide
-//             MAP_Timer_A_startCounter(TIMER_A1_BASE, TIMER_A_CONTINUOUS_MODE);
-//             MAP_Timer_A_disableInterrupt(TIMER_A1_BASE);
-//
-//             MAP_GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P3, GPIO_PIN0);
-//             MAP_GPIO_clearInterruptFlag(GPIO_PORT_P3, GPIO_PIN0);
-//             MAP_GPIO_enableInterrupt(GPIO_PORT_P3, GPIO_PIN0);
-//             MAP_GPIO_interruptEdgeSelect(GPIO_PORT_P3, GPIO_PIN0, GPIO_LOW_TO_HIGH_TRANSITION);
-//
-//   }
-//
-//
-//int main(void)
-//{
-//       uint32_t tval1, tval2;
-//       uint32_t duration;
-//
-//
-//       MAP_WDT_A_holdTimer();
-//
-//       Initalise_HCSR04();
-//
-//       MAP_Interrupt_enableInterrupt(INT_PORT3);
-//       MAP_Interrupt_enableMaster();
-//
-//       printf("Starting now \n");
-//       while(1){
-//           asm("    nop");
-//           asm("    nop");
-//           asm("    nop");
-//           if(start_pulse==1)
-//           {
-//               tval1 = MAP_Timer_A_getCounterValue(TIMER_A1_BASE);
-//
-//               while (MAP_GPIO_getInputPinValue(GPIO_PORT_P3, GPIO_PIN0) == 1);
-//
-//               tval2 = MAP_Timer_A_getCounterValue(TIMER_A1_BASE);
-//
-//               start_pulse = 0;
-//
-//               if(tval1 > tval2)
-//                   tval2 = tval2+0xffff;
-//
-//               duration = tval2-tval1;
-//               dist= duration/58.0f;
-//               printf("%d cm\n", (int)(dist));
-//
-//               if (dist < MIN_DISTANCE){
-//                   MAP_GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0);
-//               }
-//               else
-//               {
-//                   MAP_GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
-//               }
-//
-//           }
-//
-//       }
-//
-//}
-//
-//
-//void PORT3_IRQHandler(void)
-//{
-//    uint32_t status;
-//
-//    status = MAP_GPIO_getEnabledInterruptStatus(GPIO_PORT_P3);
-//    MAP_GPIO_clearInterruptFlag(GPIO_PORT_P3, status);
-//
-//    if(status & GPIO_PIN0){
-//        start_pulse=1;
-//    }
-//}
-//
-//
-//
-
+/********************************************************************************/
+/*  Ultrasonic codes for A2:                                                    */
+/*  By: CHEO CHEANG MING                                                        */
+/*      AHMAD FAIRUZI BIN KADIR SAHIB                                           */
+/*      POH KAI BOON                                                            */
+/*                                                                              */
+/*                                                                              */
+/*  Credits to Lab 4 Code Partial Reference on Ultrasonic Sensor                */
+/*                                                                              */
+/*                                                                              */
+/*                                                                              */
+/********************************************************************************/
 
 #include "driverlib.h"
 
 #define MIN_DISTANCE 15.0f // Minimum distance of the detected ultrasonic sensor
 
-
-bool start_pulse=0;
-float dist;
+bool start_pulse = 0;
+float distance = 0; // To store the converted distance
 
 const Timer_A_ContinuousModeConfig continuousModeConfig =
-   {
-            TIMER_A_CLOCKSOURCE_SMCLK,           // SMCLK Clock Source
-            TIMER_A_CLOCKSOURCE_DIVIDER_3,       // SMCLK/1 = 3MHz
-            TIMER_A_TAIE_INTERRUPT_DISABLE,      // Disable Timer ISR
-            TIMER_A_DO_CLEAR                   // Skup Clear Counter
-   };
+    {
+        TIMER_A_CLOCKSOURCE_SMCLK,      // SMCLK Clock Source
+        TIMER_A_CLOCKSOURCE_DIVIDER_3,  // SMCLK/1 = 3MHz
+        TIMER_A_TAIE_INTERRUPT_DISABLE, // Disable Timer ISR
+        TIMER_A_DO_CLEAR                // Skup Clear Counter
+};
 
-   const Timer_A_CompareModeConfig CCR1Config =
-   {
-           TIMER_A_CAPTURECOMPARE_REGISTER_1,          // Use CCR2
-           TIMER_A_CAPTURECOMPARE_INTERRUPT_DISABLE,   // Disable CCR interrupt
-           TIMER_A_OUTPUTMODE_SET_RESET,
-           0x000A                                // 10 Duty Cycle
-   };
+const Timer_A_CompareModeConfig CCR1Config =
+    {
+        TIMER_A_CAPTURECOMPARE_REGISTER_1,        // Use CCR2
+        TIMER_A_CAPTURECOMPARE_INTERRUPT_DISABLE, // Disable CCR interrupt
+        TIMER_A_OUTPUTMODE_SET_RESET,
+        0x000A // 10 Duty Cycle
+};
 
-   const Timer_A_CompareModeConfig CCR2Config =
-   {
-           TIMER_A_CAPTURECOMPARE_REGISTER_2,          // Use CCR2
-           TIMER_A_CAPTURECOMPARE_INTERRUPT_DISABLE,   // Disable CCR interrupt
-           TIMER_A_OUTPUTMODE_SET_RESET,
-           0x000A                                // 10 Duty Cycle
-   };
+const Timer_A_CompareModeConfig CCR2Config =
+    {
+        TIMER_A_CAPTURECOMPARE_REGISTER_2,        // Use CCR2
+        TIMER_A_CAPTURECOMPARE_INTERRUPT_DISABLE, // Disable CCR interrupt
+        TIMER_A_OUTPUTMODE_SET_RESET,
+        0x000A // 10 Duty Cycle
+};
+
+const Timer_A_CompareModeConfig CCR3Config =
+    {
+        TIMER_A_CAPTURECOMPARE_REGISTER_3,        // Use CCR2
+        TIMER_A_CAPTURECOMPARE_INTERRUPT_DISABLE, // Disable CCR interrupt
+        TIMER_A_OUTPUTMODE_SET_RESET,
+        0x000A // 10 Duty Cycle
+};
 
 // -------------------------------------------------------------------------------------------------------------------
-   void Initalise_HCSR04(void)
-   {
+void Initalise_HCSR04(void)
+{
 
-       /* Configuring P1.0 as output */
-       MAP_GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0); // LED lights on when lesser than minimum distance
+    /* Configuring P1.0 as output */
+    MAP_GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0); // LED lights on when lesser than minimum distance
 
+    /* For front ultrasonic sensor */
 
-       /* For front ultrasonic sensor */
+    /* Configuring P2.5 as Output */
+    MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P7, GPIO_PIN7, GPIO_PRIMARY_MODULE_FUNCTION);
+    MAP_GPIO_setAsInputPin(GPIO_PORT_P3, GPIO_PIN7);
 
-             /* Configuring P2.5 as Output */
-             MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P7, GPIO_PIN7, GPIO_PRIMARY_MODULE_FUNCTION);
-             MAP_GPIO_setAsInputPin(GPIO_PORT_P3, GPIO_PIN7);
+    /* Configuring Timer_A0 for continuous Mode */
+    MAP_Timer_A_initCompare(TIMER_A1_BASE, &CCR1Config);
+    MAP_Timer_A_configureContinuousMode(TIMER_A1_BASE, &continuousModeConfig);
+    MAP_Timer_A_startCounter(TIMER_A1_BASE, TIMER_A_CONTINUOUS_MODE);
+    MAP_Timer_A_disableInterrupt(TIMER_A1_BASE);
 
-             /* Configuring Timer_A0 for continuous Mode */
-             MAP_Timer_A_initCompare(TIMER_A1_BASE, &CCR1Config);
-             MAP_Timer_A_configureContinuousMode(TIMER_A1_BASE, &continuousModeConfig);
-             MAP_Timer_A_startCounter(TIMER_A1_BASE, TIMER_A_CONTINUOUS_MODE);
-             MAP_Timer_A_disableInterrupt(TIMER_A1_BASE);
+    MAP_GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P3, GPIO_PIN7);
+    MAP_GPIO_clearInterruptFlag(GPIO_PORT_P3, GPIO_PIN7);
+    MAP_GPIO_enableInterrupt(GPIO_PORT_P3, GPIO_PIN7);
+    MAP_GPIO_interruptEdgeSelect(GPIO_PORT_P3, GPIO_PIN7, GPIO_LOW_TO_HIGH_TRANSITION);
 
-             MAP_GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P3, GPIO_PIN7);
-             MAP_GPIO_clearInterruptFlag(GPIO_PORT_P3, GPIO_PIN7);
-             MAP_GPIO_enableInterrupt(GPIO_PORT_P3, GPIO_PIN7);
-             MAP_GPIO_interruptEdgeSelect(GPIO_PORT_P3, GPIO_PIN7, GPIO_LOW_TO_HIGH_TRANSITION);
+    /* For left ultrasonic sensor */
 
+    /* Configuring P2.5 as Output */
+    MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P7, GPIO_PIN6, GPIO_PRIMARY_MODULE_FUNCTION);
+    MAP_GPIO_setAsInputPin(GPIO_PORT_P3, GPIO_PIN0);
 
+    /* Configuring Timer_A0 for continuous Mode */
+    MAP_Timer_A_initCompare(TIMER_A1_BASE, &CCR2Config);
+    MAP_Timer_A_configureContinuousMode(TIMER_A1_BASE, &continuousModeConfig);
+    MAP_Timer_A_startCounter(TIMER_A1_BASE, TIMER_A_CONTINUOUS_MODE);
+    MAP_Timer_A_disableInterrupt(TIMER_A1_BASE);
 
-      /* For left ultrasonic sensor */
+    MAP_GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P3, GPIO_PIN0);
+    MAP_GPIO_clearInterruptFlag(GPIO_PORT_P3, GPIO_PIN0);
+    MAP_GPIO_enableInterrupt(GPIO_PORT_P3, GPIO_PIN0);
+    MAP_GPIO_interruptEdgeSelect(GPIO_PORT_P3, GPIO_PIN0, GPIO_LOW_TO_HIGH_TRANSITION);
 
-             /* Configuring P2.5 as Output */
-             MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P7, GPIO_PIN6, GPIO_PRIMARY_MODULE_FUNCTION);
-             MAP_GPIO_setAsInputPin(GPIO_PORT_P3, GPIO_PIN0);
+    /* For right ultrasonic sensor */
 
+    /* Configuring P2.5 as Output */
+    MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P7, GPIO_PIN5, GPIO_PRIMARY_MODULE_FUNCTION);
+    MAP_GPIO_setAsInputPin(GPIO_PORT_P3, GPIO_PIN6);
 
-             /* Configuring Timer_A0 for continuous Mode */
-             MAP_Timer_A_initCompare(TIMER_A1_BASE, &CCR2Config);
-             MAP_Timer_A_configureContinuousMode(TIMER_A1_BASE, &continuousModeConfig);
-             MAP_Timer_A_startCounter(TIMER_A1_BASE, TIMER_A_CONTINUOUS_MODE);
-             MAP_Timer_A_disableInterrupt(TIMER_A1_BASE);
+    /* Configuring Timer_A0 for continuous Mode */
+    MAP_Timer_A_initCompare(TIMER_A1_BASE, &CCR3Config);
+    MAP_Timer_A_configureContinuousMode(TIMER_A1_BASE, &continuousModeConfig);
+    MAP_Timer_A_startCounter(TIMER_A1_BASE, TIMER_A_CONTINUOUS_MODE);
+    MAP_Timer_A_disableInterrupt(TIMER_A1_BASE);
 
-             MAP_GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P3, GPIO_PIN0);
-             MAP_GPIO_clearInterruptFlag(GPIO_PORT_P3, GPIO_PIN0);
-             MAP_GPIO_enableInterrupt(GPIO_PORT_P3, GPIO_PIN0);
-             MAP_GPIO_interruptEdgeSelect(GPIO_PORT_P3, GPIO_PIN0, GPIO_LOW_TO_HIGH_TRANSITION);
-
-
-
-   }
-
+    MAP_GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P3, GPIO_PIN6);
+    MAP_GPIO_clearInterruptFlag(GPIO_PORT_P3, GPIO_PIN6);
+    MAP_GPIO_enableInterrupt(GPIO_PORT_P3, GPIO_PIN6);
+    MAP_GPIO_interruptEdgeSelect(GPIO_PORT_P3, GPIO_PIN6, GPIO_LOW_TO_HIGH_TRANSITION);
+}
 
 int main(void)
 {
-       uint32_t tval1, tval2;
-       uint32_t duration;
+    uint32_t tval1, tval2;
+    uint32_t duration;
 
+    MAP_WDT_A_holdTimer();
 
-       MAP_WDT_A_holdTimer();
+    Initalise_HCSR04();
 
-       Initalise_HCSR04();
+    MAP_Interrupt_enableInterrupt(INT_PORT3);
+    MAP_Interrupt_enableMaster();
 
-       MAP_Interrupt_enableInterrupt(INT_PORT3);
-       MAP_Interrupt_enableMaster();
+    printf("Starting now \n");
+    while (1)
+    {
 
-       printf("Starting now \n");
-       while(1){
-
-           //selectSensor(GPIO_PORT_P3, GPIO_PIN0, tval1,tval2,duration); // Front ultrasonic sensor
-           selectSensor(GPIO_PORT_P3, GPIO_PIN0, tval1,tval2,duration); // Left ultrasonic sensor
-       }
-
+        // selectSensor(GPIO_PORT_P3, GPIO_PIN0, tval1,tval2,duration); // Front ultrasonic sensor
+        selectSensor(GPIO_PORT_P3, GPIO_PIN0, tval1, tval2, duration); // Left ultrasonic sensor
+    }
 }
 
 void selectSensor(uint32_t GPIO_PORT, uint32_t GPIO_PIN, uint32_t tval1, uint32_t tval2, uint32_t duration)
 {
 
-       asm("    nop");
-       asm("    nop");
-       asm("    nop");
-       if(start_pulse==1) // when it is being interrupted
-       {
-           tval1 = MAP_Timer_A_getCounterValue(TIMER_A1_BASE); //The value when it hits the rising edge
+    asm("    nop");
+    asm("    nop");
+    asm("    nop");
+    if (start_pulse == 1) // when it is being interrupted
+    {
+        tval1 = MAP_Timer_A_getCounterValue(TIMER_A1_BASE); // The value when it hits the rising edge
 
-           while (MAP_GPIO_getInputPinValue(GPIO_PORT, GPIO_PIN) == 1); // While waiting for the falling edge
+        while (MAP_GPIO_getInputPinValue(GPIO_PORT, GPIO_PIN) == 1)
+            ; // While waiting for the falling edge
 
-           tval2 = MAP_Timer_A_getCounterValue(TIMER_A1_BASE); // Get the value of the falling edge
+        tval2 = MAP_Timer_A_getCounterValue(TIMER_A1_BASE); // Get the value of the falling edge
 
-           start_pulse = 0; // Set back the start pulse back to 0
+        start_pulse = 0; // Set back the start pulse back to 0
 
-           if(tval1 > tval2)
-               tval2 = tval2+0xffff; // Makes it count to the limit of the 0XFFFH to restart back to 0
+        if (tval1 > tval2)
+            tval2 = tval2 + 0xffff; // Makes it count to the limit of the 0XFFFH to restart back to 0
 
-           duration = tval2-tval1; // get the duration of the pulse
-           dist= duration/58.0f; // To convert the echo
+        duration = tval2 - tval1;    // get the duration of the pulse
+        distance = duration / 58.0f; // To convert the echo
 
-           if (dist < MIN_DISTANCE){
-               MAP_GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0);
-           }
-           else
-           {
-               MAP_GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
-           }
-
-       }
-
+        if (distance < MIN_DISTANCE)
+        { // Toggle LED if lesser than minimum distance
+            MAP_GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0);
+        }
+        else
+        {
+            MAP_GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
+        }
+    }
 }
 
 void PORT3_IRQHandler(void)
 {
-    uint32_t status;
+    uint32_t status; // local variable for interrupt status in the ISR
 
-    status = MAP_GPIO_getEnabledInterruptStatus(GPIO_PORT_P3);
-    MAP_GPIO_clearInterruptFlag(GPIO_PORT_P3, status);
+    status = MAP_GPIO_getEnabledInterruptStatus(GPIO_PORT_P3); // This API call gets the interrupt status of port 3, status of only enabled interrupts will be checked
+    MAP_GPIO_clearInterruptFlag(GPIO_PORT_P3, status);         // Clear the interrupt flag for Port 3, if not ISR will be called again as CPU assumes interrupt is not handled yet
 
-    if(status & GPIO_PIN7){
-        start_pulse=1;
+    if (status)
+    {
+        start_pulse = 1;
     }
 }
-
-
-
-
-
