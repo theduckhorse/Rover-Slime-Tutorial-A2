ef9f918b9db91317a5f396ecd7612626f6fd1e00
Hong Sheng <hongsheng322@gmail.com>
2022-11-29 22:58:08 +0800

    added more comments

--- comms/m5stickc/wifi/mqttM5.py
+++ comms/m5stickc/wifi/mqttM5.py
@@ -9,7 +9,7 @@ def lcd_print(data):
     lcd.clear(lcd.BLACK)
     lcd.text(lcd.CENTER, lcd.CENTER, data)
 
-
+# do nothing at callback
 def callback(topic_data):
   pass
 
@@ -18,6 +18,7 @@ wifiCfg.screenShow()
 # Replace "SSID" and "PASSWORD" with your WiFi SSID and Password
 wifiCfg.doConnect("username", "password")
 
+# MQTT configuration
 m5mqtt = M5mqtt(
     "mosquitto",
     "test.mosquitto.org",
@@ -38,5 +39,7 @@ message = "test"
 
 # Read/write case
 while True:
+    #sleep for 1 second
     time.sleep(1)
+    # publish to topic
     m5mqtt.publish("csc2003comms", message)
--- comms/m5stickc/wifi/udpM5.py
+++ comms/m5stickc/wifi/udpM5.py
@@ -8,25 +8,36 @@ def lcd_print(data):
     lcd.clear(lcd.BLACK)
     lcd.text(lcd.CENTER, lcd.CENTER, data)
 
+# test LCD print
 lcd_print("hello world")
+
+# connect to wifi
 wifiCfg.screenShow()
 wifiCfg.doConnect("username", "password")
 
+# set host ip address and port
 HOST = "192.168.68.117"
 PORT = 5000
 
+# set up udpsocket
 udpsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+# connect to socket
 udpsocket.connect((HOST, PORT))
 
 #udpsocket.send(DATA)
 
+# send a message to start the loop
 udpsocket.sendto("INIT", (HOST, PORT))
 
+# declare counter
 count = 0
 
 while True:
+    # receive packet
     data = udpsocket.recv(4096)
+    # increment counter and display on m5stick
     count += 1
     lcd_print(str(count))
+    # send packet back to calculate RTT
     udpsocket.send(data)
